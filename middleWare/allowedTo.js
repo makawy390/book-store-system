dc773c2ea208079ded23e843c633c308807b2f82
Mohamed Makawy <80915451+makawy390@users.noreply.github.com>
2023-11-22 13:03:00 +0200

    Update allowedTo.js

--- middleWare/allowedTo.js
+++ middleWare/allowedTo.js
@@ -3,7 +3,6 @@ const appError = require("../utils/appError");
 module.exports = (...roles)=>{
  return (req , res , next)=>{
   console.log(roles);
-  console.log(req.currentUser.role);
   if (!roles.includes(req.currentUser.role)) {
    return next(appError.create('this role is authrized' , 401))
   }
